c75bc97f890ae186cb27189cdfe2bbc4656e3b1a
Inwon Lee <freev123@naver.com>
2023-12-10 02:52:27 +0900

    .
    .

---
+++ Assets/02.Scripts/Data/Episode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Net.WebRequestMethods;
+
+
+
+[Serializable]
+public class NextClip
+{
+    [Header("연결된 클립 명")]
+    public string nextClip;
+    [Header("클릭 생성 기간 (x : 시작 시간, y : 종료 시간)")]
+    public Vector2 range;
+    [Header("클릭 위치")]
+    public Vector2 position;
+    [Header("클릭 크기")]
+    public float radius;
+}
+
+
+
+[CreateAssetMenu(fileName = "Episode", menuName= "Data/Episode")]
+public class Episode : ScriptableObject
+{
+
+    public bool isFirstClip { get { return _isFirstClip; } }
+    [SerializeField] bool _isFirstClip;
+
+    public string clipName { get { return _clipName; } }
+    [SerializeField] string _clipName;
+
+    public string defaultNextClip { get { return _defaultNextClip; } }
+    [SerializeField] string _defaultNextClip;
+
+
+    public List<NextClip> nextClips {  get { return _nextClips; } }
+    [SerializeField] List<NextClip> _nextClips;
+
+}
--- Assets/02.Scripts/Manager/VideoManager.cs
+++ Assets/02.Scripts/Manager/VideoManager.cs
@@ -8,22 +8,34 @@ using UnityEngine.Video;
 public class VideoManager : MonoBehaviour
 {
     [SerializeField] VideoClip[] sources;
+    [SerializeField] Episode[] _episodes;
 
     public Dictionary<string, VideoClip> clips { get; private set; }
+    public Dictionary<string, Episode> episodes { get; private set; }
 
     VideoPlayer videoPlayer;
-
+    Episode currentEpisode;
 
     private void Awake()
     {
         videoPlayer = GetComponent<VideoPlayer>();
 
+        videoPlayer.loopPointReached += OnEndOfClip;
+
+
+
         clips = new Dictionary<string, VideoClip>();
         for (int i = 0; i < sources.Length; i++)
-        {
-            print("sources[i].name " + sources[i].name);
             clips[sources[i].name] = sources[i];
+
+        episodes = new Dictionary<string, Episode>();
+        foreach (var it in _episodes)
+        {
+            episodes[it.clipName] = it;
+            if (it.isFirstClip) currentEpisode = it;
         }
+
+
     }
 
     int num = 1;
@@ -33,7 +45,7 @@ public class VideoManager : MonoBehaviour
         double playedTime = videoPlayer.time;
         print("playedTime " + playedTime);
         num++;
-        videoPlayer.clip = clips[((num%5) + 1).ToString()];
+        videoPlayer.clip = clips[((num % 5) + 1).ToString()];
         videoPlayer.time = playedTime;
     }
     public void ChangeClip(string clipName)
@@ -41,5 +53,13 @@ public class VideoManager : MonoBehaviour
         double playedTime = videoPlayer.time;
         videoPlayer.clip = clips[clipName];
         videoPlayer.time = playedTime;
+    }
+
+    void OnEndOfClip(VideoPlayer source)
+    {
+        Debug.Log("OnEndOfClip");
+        videoPlayer.clip = clips[currentEpisode.defaultNextClip];
+        videoPlayer.Play();
+
     }
 }
